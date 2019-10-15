af872fedb96f3eba808eb7249e1b1cdadabff925
Vladimir Makayev <vladimir.makayev@verizonconnect.com>
2019-10-15 13:13:02 +0100

    Update packages used. Fix launch settings

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,22 @@
 import * as commander from "commander";
 import { version, toolName, description } from "./metadata";
 
+(async function main() {
+    commander
+        .version(version)
+        .description(description)
+        .name(toolName)
+        .option("-p, --param1 [param1]", "Parameter1. Use your parameters here")
+        .parse(process.argv);
 
-commander
-    .version(version)
-    .description(description)
-    .name(toolName)
-    .option("-p, --param1 [param1]", "Parameter1. Use your parameters here")
-    .parse(process.argv);
+        console.log(`Add your logic to implement ${toolName}. Version : ${version}. Parameter: ${commander.param1}`);
+
+        var test = await Promise.resolve("test"); // async/await demonstration;
+
+        console.log(`Promise Resolved to ${test}`)
+})();
 
 
-console.log(`Add your logic to implement ${toolName}. Version : ${version}. Parameter: ${commander.param1}`);
 
 
 
